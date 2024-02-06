2d8b28fa87f58e08d3d4cd4f333134b96768a7bc
Max Clifford <me@mclifford.dev>
2024-02-06 20:33:17 +0000

    Added Utils

---
+++ FDOM.Rendering.Pdf/Utils.fs
@@ -0,0 +1,40 @@
+﻿namespace FDOM.Rendering.Pdf
+
+open FDOM.Core.Common
+open FreDF.Core
+
+module Utils =
+
+    let deserializeColor (value: string) =
+        let getByte (values: string array) (index: int) =
+            values
+            |> Array.tryItem index
+            |> Option.bind (fun v ->
+                match System.Byte.TryParse v with
+                | true, r -> Some r
+                | false, _ -> None)
+            |> Option.defaultValue 0uy
+
+
+        match value with
+        | _ when value.StartsWith '#' ->
+            // TODO convert hex to rgba
+            failwith "Hex not currently supported"
+
+        | _ when value.StartsWith "rgba" ->
+            let values = value.Replace("rgba(", "").Replace(")", "").Split(',')
+
+            let get = getByte values
+
+            Style.Color.RGBA(get 0, get 1, get 2, get 3)
+        | _ when value.StartsWith "rgb(" ->
+            let values = value.Replace("rgb(", "").Replace(")", "").Split(',')
+
+            let get = getByte values
+
+            Style.Color.RGBA(get 0, get 1, get 2, 0uy)
+        | _ when value.StartsWith "hsl" ->
+            let values = value.Replace("hsl(", "").Replace(")", "").Split(',')
+
+            failwith "Hsl colors not currently supported"
+        | _ -> Style.Color.Named value
