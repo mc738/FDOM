13d07ff894591d291f8ad5974a457fa4f7c0e98a
Max Clifford <me@mclifford.dev>
2024-02-12 23:56:50 +0000

    Added PreprocessorType

--- FDOM.Core/Rendering.fs
+++ FDOM.Core/Rendering.fs
@@ -4,13 +4,14 @@ module Rendering =
     
     open FDOM.Core.Common
     
-    
+    type [<RequireQualifiedAccess>] PreprocessorType =
+        | DocumentRewriter of (DOM.Document -> DOM.Document)
+        | Bespoke of (unit -> Result<unit, string>)
     
     type Preprocessor =
         {
             Name: string
             DocumentRewriter: DOM.Document -> DOM.Document
-            
         }
     
     type IRenderer =
