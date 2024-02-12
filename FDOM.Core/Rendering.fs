fe02a2747ca9e57771b346cecf0e592b50554856
Max Clifford <me@mclifford.dev>
2024-02-12 21:53:17 +0000

    Added Preprocessor

--- FDOM.Core/Rendering.fs
+++ FDOM.Core/Rendering.fs
@@ -5,6 +5,14 @@ module Rendering =
     open FDOM.Core.Common
     
     
+    
+    type Preprocessor =
+        {
+            Name: string
+            DocumentRewriter: DOM.Document -> DOM.Document
+            
+        }
+    
     type IRenderer =
 
         abstract member Run: DOM.Document -> unit
